4f53714141981efba16989ab65d1e78cdae06fa1
Barbone <christian_i_ionescu@yahoo.com>
2022-08-04 22:49:05 +0300

    Comit message testing and learning

---
+++ myprogram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//Program de  calculator  în C;
+/* Acesta este un comentariu multi liniar
+pot sa am oricate linii de comentariu
+*/
+int main(void){
+  printf("\n");
+char cuvant[]="Material";
+printf("And the word is \t %s", cuvant);
+printf("\n");
+printf("\n\n");
+}
