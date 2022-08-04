c412e7088308185e2dce05cf43598b9931696d1f
Barbone <christian_i_ionescu@yahoo.com>
2022-08-04 23:05:13 +0300

    Learned multidimensional arrays

--- myprogram.c
+++ myprogram.c
@@ -4,9 +4,17 @@
 pot sa am oricate linii de comentariu
 */
 int main(void){
-  printf("\n");
-char cuvant[]="Material";
-printf("And the word is \t %s", cuvant);
 printf("\n");
+printf("\n");
+
+
+int myarr[2][3]={{1,2,3}, {4,5,6}};
+for(int i= 0; i<2; i++){
+  for(int j=0; j<3; j++){
+    printf("%d", myarr[i][j]);
+  }
+  printf("\n\n");
+}
+
 printf("\n\n");
 }
